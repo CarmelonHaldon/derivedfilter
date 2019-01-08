74dec15a9110106cce89725b8c24975ffe3f8e0d
CarmelonHaldon <carmelonhaldon@gmail.com>
2019-01-08 17:35:41 +0100

    Primera subida...

---
+++ src/carmelonhaldon/derivedfilter/DerivedFilter.java
@@ -0,0 +1,32 @@
+package carmelonhaldon.derivedfilter;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+
+public class DerivedFilter extends ViewerFilter {
+
+	public boolean select(Viewer arg0, Object arg1, Object element) {
+
+		// if (element instanceof ICompilationUnit) {
+		// try {
+		// ICompilationUnit cu = (ICompilationUnit) element;
+		// if (cu.getUnderlyingResource() != null) {
+		// return !ROO_AJ_PATTERN.matcher(cu.getUnderlyingResource().getName()).matches();
+		// }
+		// }
+		// catch (JavaModelException e) {
+		// // just ignore this here
+		// }
+		// }
+		// return true;
+
+		if (!(element instanceof IResource)) {
+			return true;
+		}
+
+		IResource resource = (IResource) element;
+
+		return !resource.isDerived();
+	}
+}
